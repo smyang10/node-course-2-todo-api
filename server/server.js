9be91442d864d5c07b10b3c9a8e76ca826952c19
Stuart Yang <smyang10@gmail.com>
2017-02-04 14:53:27 -0800

    fixed port again

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT  || 3000;
 
 app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
