f45d830dbcb9b06c6dffe58cf59779aab216b030
Stuart Yang <smyang10@gmail.com>
2017-02-05 14:01:33 -0800

    fixed mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://todoadmin:todoadmin@ds111589.mlab.com:11589/todoapp'
-};
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://todoadmin:todoadmin@ds111589.mlab.com:11589/todoapp'
+// };
 
 mongoose.Promise = global.Promise;
-//mongoose.connect( db.mlab || db.localhost);
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
